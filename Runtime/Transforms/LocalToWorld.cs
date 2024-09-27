027ab89621b017348e40bec559c4b0b6931b3c96
ChaosVan <reyu8019@vip.qq.com>
2024-09-27 10:53:48 +0800

    opt localtoworld

--- Runtime/Transforms/LocalToWorld.cs
+++ Runtime/Transforms/LocalToWorld.cs
@@ -5,28 +5,12 @@ namespace Entities
 {
     public class LocalToWorld : IComponentData, IDisposable
     {
-        private const float EPSILON = 1e-6f;
         public float4x4 Value = float4x4.identity;
         public float3 Right => Value.c0.xyz;
         public float3 Up => Value.c1.xyz;
         public float3 Forward => Value.c2.xyz;
         public float3 Position => Value.c3.xyz;
-        public quaternion Rotation
-        {
-            get
-            {
-                var m = new float3x3(Value);
-                if (math.determinant(m) < 0)
-                {
-                    var tmp = math.mulScale(m, 1 / Scale);
-                    if (math.abs(1.0f - math.determinant(tmp)) < EPSILON)
-                        return new quaternion(tmp);
-                }
-
-                return math.rotation(m);
-                // return new quaternion(math.orthonormalize(m));
-            }
-        }
+        public quaternion Rotation => math.rotation(math.mulScale(new float3x3(Value), 1 / Scale));
         public float3 Scale
         {
             get
